3e3475fada41d64dbb17b4eb594f5539e5ceda65
Dominik Janik <mortyymer@gmail.com>
2019-11-18 22:20:04 +0100

    'cleanup'

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -11,17 +11,8 @@ async function register(userData) {
 async function getUserByLogin(userName): Promise<any[]> {
     return await dbService.get({userName}, dbService.databaseTables.users);
 }
-
-async function login(userData) {
-    return await dbService.get({
-        userLogin: userData.login,
-        password: userData.passwordHash
-    }, dbService.databaseTables.users);
-}
-
 export {
     getUserByLogin,
     register,
-    login,
     getUserData
 };
